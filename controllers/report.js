6d42f454b23a9b3aa783a05e39df220383bd64a5
JTsquared <turner.e.jonathan@gmail.com>
2022-03-08 17:00:55 -0500

    added functionality to add and retrieve reports

--- controllers/report.js
+++ controllers/report.js
@@ -3,8 +3,9 @@ const {validationResult} = require('express-validator')
 const pushToQueue = require('../helpers/pushToQueue.js');
 const processQueue = require('../helpers/processQueue');
 const { detectFace } = require('./azureFace.js');
+const Report = require('../models/Report.js');
 
-exports.report = async(req, res)=> {
+exports.addReport = async(req, res)=> {
 
     const errors = validationResult(req)
 
@@ -15,7 +16,8 @@ exports.report = async(req, res)=> {
         })
     }
 
-    const {findName, reporterName, providerId, image, email} = req.body
+    const { missingName, missingImage, reporterId, reporterName, reporterImage, reporterEmail, notificationToken } = req.body
+    console.log(req.body);
 
     try{
         //TODO:
@@ -23,14 +25,30 @@ exports.report = async(req, res)=> {
         //convert image from active report to byte array
         //convert all images retrieved from the database to byte arrays
         //call azure face detect api on all images to obtain faceIds
-        const faceId1 = detectFace()
+        //const faceId1 = detectFace()
         //push images to face verify queue in mongodb then immediately attempt to process task
-        pushToQueue(providerId, )
-        processQueue()
+        //pushToQueue(providerId, )
+        //processQueue()
+        var matchFound = false;
+
+        //if no match is found add message to report database
+        if (!matchFound){
+            Report.create({
+                missingName,
+                missingImage,
+                reporterId,
+                reporterName,
+                reporterImage,
+                reporterEmail,
+                notificationToken
+            })
+        }
+
+        const message = matchFound ? 'Good news we have found a match!' : 'Your report has been submitted, Thank you'
 
         res.status(200).json({
             success: true,
-            message: 'Your report has been submitted, Thank you'
+            message: message
         })
     }
     catch(error){
@@ -41,3 +59,25 @@ exports.report = async(req, res)=> {
         })
     }
 }
+
+exports.getReports = async(req, res) => {
+    const userId = req.user
+
+    try{
+        //TODO: right now we have no way to filter by location and will need to add coordinates to filter results based on proximity
+        const reports = await Report.find();
+
+        res.status(200).json({
+            success: true,
+            reports
+        })
+    }
+    catch(error){
+        console.error(error.message)
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error!!'
+        })
+    }
+} 
+
--- routes/report.js
+++ routes/report.js
@@ -2,17 +2,19 @@ const express = require('express')
 const router = express.Router()
 const {body} = require('express-validator')
 const {fetchUser} = require('../middlewares/fetchUser')
-const {report} = require('../controllers/report')
-const {verifyFace} = require('../controllers/report')
+const {addReport, getReports} = require('../controllers/report')
 
-router.post('/report', fetchUser, [
-    body('findName').isLength({min: 3, max: 40}).withMessage('Name should be at least 3 to 40 characters long!!'),
+router.post('/report/addReport', fetchUser, [
+    body('missingName').isLength({min: 3, max: 40}).withMessage('Name should be at least 3 to 40 characters long!!'),
+    body('missingImage').isLength({min: 1}).withMessage('Image is required!!'),
+    body('reporterId').isLength({min: 1}).withMessage('Provider id is required!!'),
     body('reporterName').isLength({min: 3, max: 40}).withMessage('Name should be at least 3 to 40 characters long!!'),
-    body('providerId').isLength({min: 1}).withMessage('Provider id is required!!'),
-    body('image').isLength({min: 1}).withMessage('Image is required!!'),
-    body('email').isEmail().withMessage('Valid Email is required!!'),
-], report)
+    body('reporterImage').isLength({min: 1}).withMessage('Image is required!!'),
+    body('reporterEmail').isEmail().withMessage('Valid Email is required!!'),
+    body('notificationTokens').isLength({min: 1, max: 40}).withMessage("Notification token is required")
+], addReport)
 
+router.get('/report/getReports', fetchUser, getReports)
 
 
 module.exports = router
